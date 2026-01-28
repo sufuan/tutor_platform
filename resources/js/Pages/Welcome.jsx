ca89af6ee9c7830d992241da9034e17da9a7b11a
sufuan <abu49539@gmail.com>
2026-01-28 16:32:18 +0600

    j

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -86,7 +86,7 @@ export default function Welcome({
                         {/* Left Content */}
                         <div className="space-y-8">
                             <h1 className="text-5xl lg:text-7xl font-black text-slate-900 leading-tight">
-                                Find the
+                                Find the fucking
                                 <span className="block text-[#0675C1]">
                                     Perfect Tutor
                                 </span>
