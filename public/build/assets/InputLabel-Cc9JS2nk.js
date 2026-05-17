168566ceec2c4279af1641c2e63de28b3369f5c5
sufuan <abu49539@gmail.com>
2026-05-17 21:38:37 +0600

    minor issue

---
+++ public/build/assets/InputLabel-Cc9JS2nk.js
@@ -0,0 +1,1 @@
+import{j as m}from"./app-7fj3pjPF.js";function o({value:t,className:e="",children:r,...s}){return m.jsx("label",{...s,className:"block text-sm font-medium text-gray-700 "+e,children:t||r})}export{o as I};
